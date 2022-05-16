db60fc5fc59ab40c4481f15b023db994d3230855
Ophen <itsophen@gmail.com>
2022-05-16 18:51:29 +0100

    fix leaks and everything works dynamically now

--- class-training-00/code.c
+++ class-training-00/code.c
@@ -22,53 +22,84 @@ void myfunc(person *p) {
     printf("%s is %d years old\n", p->name, p->age);
 }
 
+void free_array(void **ptr, int size){
+	int i = 0;
+	for (; i < size; i++)
+		free(ptr[i]);
+	free(ptr);
+}
+
+person *copy_class_person(person *ptr)
+{
+	person *p = malloc(sizeof(person));
+	p->name = ptr->name;
+	p->age = ptr->age;
+	return p;
+}
+
 int main()
 {
-    person p;
-    __init__person_class(&p, "Ophen", 19);
-    myfunc(&p);
+    person *p = NULL;
+	p = malloc(sizeof(person));
+    __init__person_class(p, "Ophen", 19);
+    myfunc(p);
 
-    p.age = 20;
-    myfunc(&p);
+    p->age = 20;
+    myfunc(p);
 
-    person p2;
-    __init__person_class(&p2, "Saad", 22);
-    printf("the combined age of %s and %s is %d\n", p.name, p2.name, p.age + p2.age);
+    person *p2 = NULL;
+	p2 = malloc(sizeof(person));
+    __init__person_class(p2, "Saad", 22);
+    printf("the combined age of %s and %s is %d\n", p->name, p2->name, p->age + p2->age);
 
-    if (p.age > p2.age)
-        printf("%s is older than %s\n", p.name, p2.name);
+    if (p->age > p2->age)
+        printf("%s is older than %s\n", p->name, p2->name);
     else
-        printf("%s is older than %s\n", p2.name, p.name);
+        printf("%s is older than %s\n", p2->name, p->name);
 
-    person *persons;
-    int __size_of_persons = 0;
-
-    __size_of_persons += 1;
-    persons = realloc(persons ,__size_of_persons * sizeof(person));
-    person person_tmp;
-    __init__person_class(&person_tmp, "Jawad", 99);
-    persons[0] = person_tmp;
-    myfunc(&persons[0]);
+    person **persons = NULL;
+    int __size_of_persons = 1;
+    person *person_tmp = NULL;
 
     __size_of_persons += 1;
-    persons = realloc(persons ,__size_of_persons * sizeof(person));
-    __init__person_class(&person_tmp, "rida", 55);
-    persons[1] = person_tmp;
-    myfunc(&persons[1]);
+    persons = realloc(persons ,(__size_of_persons + 1) * sizeof(person *));
+	persons[__size_of_persons] = NULL;
+	person_tmp = malloc(sizeof(person));
+    __init__person_class(person_tmp, "Jawad", 99);
+    persons[0] = copy_class_person(person_tmp);
+	free(person_tmp);
+    myfunc(persons[0]);
 
     __size_of_persons += 1;
-    persons = realloc(persons ,__size_of_persons * sizeof(person));
-    persons[2] = p;
-    myfunc(&persons[2]);
+    persons = realloc(persons ,(__size_of_persons + 1) * sizeof(person *));
+	persons[__size_of_persons] = NULL;
+	person_tmp = malloc(sizeof(person));
+    __init__person_class(person_tmp, "rida", 55);
+    persons[1] = copy_class_person(person_tmp);
+	free(person_tmp);
+    myfunc(persons[1]);
+
+    __size_of_persons += 1;
+    persons = realloc(persons ,(__size_of_persons + 1) * sizeof(person *));
+	persons[__size_of_persons] = NULL;
+    persons[2] = copy_class_person(p);
+    myfunc(persons[2]);
     
     for (int i = 1;i < __size_of_persons - 1;i++)
+	{
+		free(persons[i]);
         persons[i] = persons[i + 1];
-    __size_of_persons -= 1;
-    persons = realloc(persons ,__size_of_persons * sizeof(person));
-    myfunc(&persons[1]);
+    }
+	__size_of_persons -= 1;
+    persons = realloc(persons ,(__size_of_persons + 1) * sizeof(person *));
+	persons[__size_of_persons] = NULL;
+    myfunc(persons[1]);
 
-	__call__person_class(&persons[0], "Rina", 18);
-	myfunc(&persons[0]);
-    free(persons);
-    return 0;
+	__call__person_class(persons[0], "Rina", 18);
+	myfunc(persons[0]);
+
+	free_array((void **)persons, __size_of_persons);
+    free(p);
+    free(p2);
+	return 0;
 }
