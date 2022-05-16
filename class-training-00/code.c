4029a7abe3f1a0411bf5ad939a631cde6a03664a
Ophen <itsophen@gmail.com>
2022-05-16 19:09:01 +0100

    added comments for more clarity

--- class-training-00/code.c
+++ class-training-00/code.c
@@ -8,20 +8,24 @@ typedef struct t_person {
     int age;
 } person;
 
+// __init__ magic method
 void __init__person_class(person *p, char *name, int age) {
     p->name = name;
     p->age = age;
 }
 
+// __call__ magic method
 void __call__person_class(person *p, char *name, int age) {
 	p->name = name;
 	p->age = age;
 }
 
+// method
 void myfunc(person *p) {
     printf("%s is %d years old\n", p->name, p->age);
 }
 
+// free array of pointers
 void free_array(void **ptr, int size){
 	int i = 0;
 	for (; i < size; i++)
@@ -29,6 +33,7 @@ void free_array(void **ptr, int size){
 	free(ptr);
 }
 
+// copy an object and return the new allocated copy
 person *copy_class_person(person *ptr)
 {
 	person *p = malloc(sizeof(person));
@@ -39,49 +44,71 @@ person *copy_class_person(person *ptr)
 
 int main()
 {
+	// p = person("Ophen",19)
     person *p = NULL;
 	p = malloc(sizeof(person));
     __init__person_class(p, "Ophen", 19);
+
+	// p.myfunc()
     myfunc(p);
 
+    // p.age = 20;
     p->age = 20;
+
+	// p.myfunc()
     myfunc(p);
 
+	// p2 = person("Saad",22)
     person *p2 = NULL;
 	p2 = malloc(sizeof(person));
     __init__person_class(p2, "Saad", 22);
+	
+	// print(f"the combined age of {p.name} and {p2.name} is {p.age + p2.age}")
     printf("the combined age of %s and %s is %d\n", p->name, p2->name, p->age + p2->age);
 
+	// if p.age > p2.age:
     if (p->age > p2->age)
+		// print(f"{p.name} is older than {p2.name}")
         printf("%s is older than %s\n", p->name, p2->name);
     else
+		// print(f"{p2.name} is older than {p.name}")
         printf("%s is older than %s\n", p2->name, p->name);
 
+	// persons = []
     person **persons = NULL;
     int __size_of_persons = 0;
     person *person_tmp = NULL;
 
+	// persons.append(person("Jawad",99))
     __size_of_persons += 1;
     persons = realloc(persons ,(__size_of_persons) * sizeof(person *));
 	person_tmp = malloc(sizeof(person));
     __init__person_class(person_tmp, "Jawad", 99);
     persons[0] = copy_class_person(person_tmp);
 	free(person_tmp);
+
+	// persons[0].myfunc();
     myfunc(persons[0]);
 
+	// persons.append(person("rida",55))
     __size_of_persons += 1;
     persons = realloc(persons ,(__size_of_persons) * sizeof(person *));
 	person_tmp = malloc(sizeof(person));
     __init__person_class(person_tmp, "rida", 55);
     persons[1] = copy_class_person(person_tmp);
 	free(person_tmp);
+
+	// persons[1].myfunc();
     myfunc(persons[1]);
 
+	// persons.append(p)
     __size_of_persons += 1;
     persons = realloc(persons ,(__size_of_persons) * sizeof(person *));
     persons[2] = copy_class_person(p);
+
+	// persons[2].myfunc();
     myfunc(persons[2]);
-    
+    // del persons[1]
     for (int i = 1;i < __size_of_persons - 1;i++)
 	{
 		free(persons[i]);
@@ -89,13 +116,19 @@ int main()
     }
 	__size_of_persons -= 1;
     persons = realloc(persons ,(__size_of_persons) * sizeof(person *));
+	// persons[1].myfunc();
     myfunc(persons[1]);
 
+	// persons[0]("Rina", 18);
 	__call__person_class(persons[0], "Rina", 18);
+
+	// persons[0].myfunc();
 	myfunc(persons[0]);
 
+	// freeing all allocated memory
 	free_array((void **)persons, __size_of_persons);
     free(p);
     free(p2);
-	return 0;
+
+	return (0);
 }
